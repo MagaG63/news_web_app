f3058e06f344cd451c14efa877182ef9aeb896b7
MagaG63 <magakadim11@gmail.com>
2026-05-13 20:47:51 +0300

    done app

---
+++ news_app/src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,15 @@
+import { formatTimeAgo } from "../../helpers/formatTimeAgo.js";
+import Image from "../Image/Image.jsx";
+import styles from "./styles.module.css";
+
+export default function NewsBanner({ item }) {
+  return (
+    <div className={styles.display}>
+      <Image image={item?.image} />
+      <h3 className={styles.title}>{item.title}</h3>
+      <p className={styles.extra}>
+        {formatTimeAgo(item.published)} by {item.author}
+      </p>
+    </div>
+  );
+}
